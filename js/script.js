1cc0caaa8f74b8959cad48c0585b68bd58b1a40b
Sean Wines <seanwinesdev@gmail.com>
2016-08-21 14:12:57 -0700

    Added nav spy

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 $(window).on('scroll', function(event){
-  var postion = $(this).scrollTop();
-  console.log(postion);
+  var position = $(this).scrollTop();
 
   // hide brand when scroll down and show when at the top
-  if (postion > 15) {
+  if (position > 15) {
     $('.main-nav').addClass('nav-scroll-up');
   } else {
     $('.main-nav').removeClass('nav-scroll-up');
   }
 
+  //nav spy
+  if (position < $('#menu').offset().top) {
+    removeSelected();
+    $('.home-nav').addClass('selected');
+  }
+  if (position > $('#menu').offset().top) {
+    removeSelected();
+    $('.menu-nav').addClass('selected');
+  }
+
+  if (position > $('#about').offset().top - 100) {
+    removeSelected();
+    $('.about-nav').addClass('selected');
+  }
+
+  if (position > $('#contact').offset().top - 200) {
+    removeSelected();
+    $('.contact-nav').addClass('selected');
+  }
+
 });
+
+var removeSelected = function () {
+  $('li').removeClass('selected');
+}
+
+var addSelected = function (item) {
+  item.addClass('selected');
+}
